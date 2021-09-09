8e40d674b1913227e2f3a628667912e2369c747d
koken <johnmalay5518@gmail.com>
2021-09-09 17:00:01 +0800

    Update footer.php

--- web/footer.php
+++ web/footer.php
@@ -1,3 +1,3 @@
 <footer class="text-center">
-    © 2021 <a href="https://github.com/lutfailham96/libernet">Libernet</a> v1.5.1. All rights reserved.
+    © 2021 <a href="https://github.com/lutfailham96/libernet">Libernet</a> v1.5.1. All rights reserved. Fork by gghehe
 </footer>
